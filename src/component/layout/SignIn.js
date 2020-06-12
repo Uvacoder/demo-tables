a5293a461b15581b376228beede753035018139f
jideabdqudus <jideabdqudus@gmail.com>
2020-06-12 18:11:54 +0100

    Refactored Signin

--- src/component/layout/SignIn.js
+++ src/component/layout/SignIn.js
@@ -1,56 +1,56 @@
 import React from "react";
+import ja from "./ja21.png"
 
 const SignIn = () => {
   return (
     <div
       style={{
-        paddingTop: "200px",
-        paddingBottom: "200px",
-        backgroundColor:"#f5f5f5",
-        height:"100%",
+        paddingTop: "150px",
+        paddingBottom: "150px",
+        height: "100%",
         alignItems: "center",
       }}
     >
-      <div class="text-center">
-        <form class="form-signin">
+      <div className="text-center">
+        <form className="form-signin">
           <img
-            class="mb-4"
-            src="../assets/brand/bootstrap-solid.svg"
+            className="mb-4"
+            src={ja}
             alt=""
-            width="72"
-            height="72"
+            width="100"
+            height="100"
           />
-          <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-          <label for="inputEmail" class="sr-only">
+          <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+          <label for="inputEmail" className="sr-only">
             Email address
           </label>
           <input
             type="email"
             id="inputEmail"
-            class="form-control"
+            className="form-control"
             placeholder="Email address"
             required
             autofocus
           />
-          <label for="inputPassword" class="sr-only">
+          <label for="inputPassword" className="sr-only">
             Password
           </label>
           <input
             type="password"
             id="inputPassword"
-            class="form-control"
+            className="form-control"
             placeholder="Password"
             required
           />
-          <div class="checkbox mb-3">
+          <div className="checkbox mb-3">
             <label>
               <input type="checkbox" value="remember-me" /> Remember me
             </label>
           </div>
-          <button class="btn btn-lg btn-primary btn-block" type="submit">
+          <button className="btn btn-lg btn-primary btn-block" type="submit">
             Sign in
           </button>
-          <p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
+          <p className="mt-5 mb-3 text-muted">&copy; 2020</p>
         </form>
       </div>
     </div>
