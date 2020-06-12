6ee4c230339c868694d1859c83f441079941c46c
jideabdqudus <jideabdqudus@gmail.com>
2020-06-12 18:01:18 +0100

    Deleted dashboard.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavbarItem from "./component/Navigations/NavbarItem";
 import Sidebar from "./component/Navigations/Sidebar";
-import Dashboard from "./component/layout/Dashboard";
 import FormItem from "./component/layout/Form";
 import DashItem from "./component/layout/Table";
 import SignIn from "./component/layout/SignIn";
@@ -32,7 +31,6 @@ function App() {
             <Fragment>
               <NavbarItem />
               <Sidebar />
-              <Dashboard />
               <FormItem />
             </Fragment>
           )}
