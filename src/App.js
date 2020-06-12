54077e405e3cdcdec637c2677d7e12f4f3685496
jideabdqudus <jideabdqudus@gmail.com>
2020-06-12 17:12:23 +0100

    Login Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import NavbarItem from "./component/Navigations/NavbarItem";
 import Sidebar from "./component/Navigations/Sidebar";
 import Dashboard from "./component/layout/Dashboard";
 import FormItem from "./component/layout/Form";
 import DashItem from "./component/layout/Table";
+import SignIn from "./component/layout/SignIn";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={SignIn}/>
+      <Route exact path ="/login" component={FormItem}/>
       <NavbarItem />
       <Sidebar />
       <Dashboard />
-      <FormItem />
       <DashItem />
-    </div>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/component/layout/Dashboard.js
+++ src/component/layout/Dashboard.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4" style={{marginTop:"110px"}}>
+    <div
+      role="main"
+      class="col-md-9 ml-sm-auto col-lg-10 px-md-4"
+      style={{ marginTop: "110px" }}
+    >
       <main>
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 class="h2">Form Inputs</h1>
