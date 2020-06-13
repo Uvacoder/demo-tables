8fe23403a015360754e317cb987f47f45e812f4f
jideabdqudus <jideabdqudus@gmail.com>
2020-06-13 18:33:56 +0100

    Initializing Context

--- src/component/layout/FormItem.js
+++ src/component/layout/FormItem.js
@@ -1,40 +1,95 @@
 import React from "react";
-import { Col, Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 const FormItem = (props) => {
   return (
     <div>
       <div>
-        <main>
           <div>
             <h1 className="h2">Form Inputs</h1>
           </div>
-        </main>
       </div>
-      <Form>
-        <Col>
-          <FormGroup>
-            <Label for="exampleDate">Date</Label>
-            <Input
-              type="date"
-              name="date"
-              required
-              placeholder="Input date here"
-            />
-          </FormGroup>
-        </Col>
-        <Col>
-          <FormGroup>
-            <Label for="exampleName">Item name</Label>
-            <Input
-              type="text"
-              required
-              name="name"
-              placeholder="Input item name here"
-            />
-          </FormGroup>
-        </Col>
-        <Button color="success">Submit</Button>
+      <Form className="container formTop">
+        <FormGroup>
+          <Label for="exampleDate">Date</Label>
+          <Input
+            type="date"
+            name="date"
+            required
+            id="exampleDate"
+            placeholder="Input date here"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleName">Item name</Label>
+          <Input
+            type="text"
+            required
+            name="name"
+            id="exampleName"
+            placeholder="Input item name here"
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="exampleNumber">Opening stock</Label>
+          <Input
+            type="number"
+            name="number"
+            required
+            id="exampleNumber"
+            placeholder="Input opening stock here"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleNumber">Daily production</Label>
+          <Input
+            type="number"
+            name="number"
+            required
+            id="exampleNumber"
+            placeholder="Input Daily production here"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleNumber">Closing stock</Label>
+          <Input
+            type="number"
+            name="number"
+            required
+            id="exampleNumber"
+            placeholder="Input closing stock here"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleNumber">Price</Label>
+          <Input
+            type="number"
+            name="number"
+            required
+            id="exampleNumber"
+            placeholder="Input Price here"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleNumber">Your name</Label>
+          <Input
+            type="number"
+            name="number"
+            required
+            id="exampleNumber"
+            placeholder="Input Your name here"
+          />
+        </FormGroup>
+        <FormGroup check>
+          <Input type="checkbox" name="check" id="exampleCheck" />
+          <Label for="exampleCheck" check required>
+            Admin Verified
+          </Label>
+        </FormGroup>
+        <Button color="success" block>
+          Submit
+        </Button>
       </Form>
     </div>
   );
