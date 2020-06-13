036b545a64beb9c981775304246bd0c1a6d542a4
jideabdqudus <jideabdqudus@gmail.com>
2020-06-13 18:44:28 +0100

    sET TYPES

---
+++ src/context/Types.js
@@ -0,0 +1,10 @@
+export const ADD_ITEM = "ADD_ITEM";
+export const DELETE_ITEM = "DELETE_ITEM";
+export const EDIT_ITEM = "EDIT_ITEM";
+export const DELETE_ITEM = "DELETE_ITEM";
+export const FILTER_ITEM = "FILTER_ITEM";
+export const SET_CURRENT = "SET_CURRENT";
+export const CLEAR_CURRENT = "CLEAR_CURRENT";
+export const CLEAR_FILTER = "CLEAR_FILTER";
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
