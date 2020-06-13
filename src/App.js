24756914945c8b5b4303cbf0c470a797e941dcda
jideabdqudus <jideabdqudus@gmail.com>
2020-06-13 16:57:19 +0100

    New refactoring

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import NavbarItem from "./component/navigations/navbar/NavbarItem";
-import FormItem from "./component/layout/Form";
-import TableItem from "./component/layout/Table";
 import Login from "./component/pages/login/Login";
 import Register from "./component/pages/register/Register";
-import { Row } from "reactstrap";
+import Home from "./component/pages/land/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
+      <Fragment>
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-        <Route
-          exact
-          path="/"
-          render={(props) => (
-            <Fragment>
-              <NavbarItem />
-              <Row xs="12">
-              <FormItem />
-              <TableItem />
-              </Row>
-            </Fragment>
-          )}
-        />
-        <Route
-          exact
-          path="/form"
-          render={(props) => (
-            <Fragment>
-              <NavbarItem />
-              <FormItem />
-            </Fragment>
-          )}
-        />
+        <Route exact path="/" component={Home} />
+        </Fragment>
       </Switch>
     </BrowserRouter>
   );
---
+++ src/component/pages/land/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import FormItem from "../../layout/FormItem";
+import TableItem from "../../layout/TableItem";
+import NavbarItem from "../../navigations/navbar/NavbarItem";
+
+const Home = () => {
+  return (
+    <div>
+      <NavbarItem />
+      <FormItem />
+      <TableItem />
+    </div>
+  );
+};
+
+export default Home;
